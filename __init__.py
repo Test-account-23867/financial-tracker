22bc09e25ae730bb78e6742cea6af2baa46dde4f
Test-account-23867 <120910701+Test-account-23867@users.noreply.github.com>
2025-03-10 01:37:06 +0530

    Update __init__.py to use env var

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.secret_key = 'ae&*T^#OYTGrBLHRYYYYGOUihef'
+app.secret_key = os.environ.get('APP_SECRET_KEY')
 
 db = SQLAlchemy(app)
 
